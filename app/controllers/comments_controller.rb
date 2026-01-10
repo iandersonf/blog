0e1c2d3707aa5a9c3d4347b196b4c91af813e95d
Ianderson Farias <iandersoncounter@gmail.com>
2026-01-10 16:46:01 -0300

    adicionando comentários, Broadcasts e configurando producao

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+  before_action :set_post
+
+  def create
+    @comment = @post.comments.build(comment_params)
+
+    if @comment.save
+      redirect_to @post, notice: "Comment was successfully created."
+    else
+      redirect_to @post, alert: "Error creating comment."
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
